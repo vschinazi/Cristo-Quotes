5c2388969af6701ffc906af52ca91eba19674754
Victor Schinazi <v.schinazi@gmail.com>
2025-03-17 16:57:47 +0800

    Updated MonteCristoQuotesApp.swift and project settings

--- MonteCristoQuotes/MonteCristoQuotes/MonteCristoQuotesApp.swift
+++ MonteCristoQuotes/MonteCristoQuotes/MonteCristoQuotesApp.swift
@@ -14,19 +14,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem?
     private var quoteTimer: Timer?
     private var dailyTimers: [Timer] = []
-    
+    private var nextQuoteItem: NSMenuItem!
+
     private enum ScheduleOption: String {
         case every30Seconds = "Every 30 Seconds (Testing)"
         case every30Minutes = "Every 30 Minutes"
         case everyHour = "Every Hour"
         case fixedDailyTimes = "At 09:00, 12:00, 16:00, 20:00, 23:00"
     }
-    
-    private var selectedOption: ScheduleOption = .fixedDailyTimes  // Set default to fixed times
+
+    private var selectedOption: ScheduleOption = .fixedDailyTimes
+    private var nextQuoteTime: Date? // Store the next quote time
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupMenuBar()
         scheduleFixedDailyTimes(hours: [9, 12, 16, 20, 23]) // Default schedule
+        updateNextQuoteDisplay()  // Ensure next quote is displayed immediately
     }
 
     private func setupMenuBar() {
@@ -42,7 +45,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let everyHourItem = NSMenuItem(title: ScheduleOption.everyHour.rawValue, action: #selector(hourly), keyEquivalent: "")
         let fixedTimesItem = NSMenuItem(title: ScheduleOption.fixedDailyTimes.rawValue, action: #selector(fixedDailyTimes), keyEquivalent: "")
 
-        // Add checkmarks to the currently selected option
         every30SecItem.state = (selectedOption == .every30Seconds) ? .on : .off
         every30MinItem.state = (selectedOption == .every30Minutes) ? .on : .off
         everyHourItem.state = (selectedOption == .everyHour) ? .on : .off
@@ -52,11 +54,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menu.addItem(every30MinItem)
         menu.addItem(everyHourItem)
         menu.addItem(fixedTimesItem)
+        menu.addItem(.separator())
+
+        nextQuoteItem = NSMenuItem(title: "Next Quote: Calculating...", action: nil, keyEquivalent: "")
+        nextQuoteItem.isEnabled = false
+        menu.addItem(nextQuoteItem)
 
         menu.addItem(.separator())
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
 
         statusItem?.menu = menu
+
+        updateNextQuoteDisplay()
     }
 
     // MARK: - Scheduling Actions
@@ -92,9 +101,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         dailyTimers.forEach { $0.invalidate() }
         dailyTimers.removeAll()
 
+        nextQuoteTime = Date().addingTimeInterval(interval) // Store the next quote time
+
         quoteTimer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
             self.showQuote()
         }
+
+        updateNextQuoteDisplay()
     }
 
     private func scheduleFixedDailyTimes(hours: [Int]) {
@@ -105,33 +118,59 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let calendar = Calendar.current
         let now = Date()
 
+        var nextTime: Date?
         for hour in hours {
-            var nextTrigger = calendar.date(bySettingHour: hour, minute: 0, second: 0, of: now)!
-
-            if nextTrigger <= now {
-                nextTrigger = calendar.date(byAdding: .day, value: 1, to: nextTrigger)!
+            if let scheduledTime = calendar.date(bySettingHour: hour, minute: 0, second: 0, of: now),
+               scheduledTime > now {
+                nextTime = scheduledTime
+                break
             }
+        }
 
-            let timer = Timer(fireAt: nextTrigger, interval: 86400, target: self, selector: #selector(showQuote), userInfo: nil, repeats: true)
+        if nextTime == nil, let firstTimeTomorrow = calendar.date(bySettingHour: hours.first!, minute: 0, second: 0, of: Calendar.current.date(byAdding: .day, value: 1, to: now)!) {
+            nextTime = firstTimeTomorrow
+        }
+
+        if let nextTime = nextTime {
+            nextQuoteTime = nextTime // Store the next quote time
+            let timer = Timer(fireAt: nextTime, interval: 86400, target: self, selector: #selector(showQuote), userInfo: nil, repeats: true)
             RunLoop.main.add(timer, forMode: .common)
             dailyTimers.append(timer)
         }
+
+        updateNextQuoteDisplay()
     }
 
-    // MARK: - Menu Refresh
+    // MARK: - Update Next Quote Time in Menu
 
-    private func refreshMenu() {
-        setupMenuBar()  // Rebuild the menu to update checkmarks
+    private func updateNextQuoteDisplay() {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+
+        DispatchQueue.main.async {
+            if let nextTime = self.nextQuoteTime {
+                self.nextQuoteItem.title = "Next Quote: \(formatter.string(from: nextTime))"
+            } else {
+                self.nextQuoteItem.title = "Next Quote: Error Calculating"
+            }
+        }
     }
 
     // MARK: - Basic Actions
 
     @objc func showQuote() {
         QuotePopupManager.showPopup()
+        nextQuoteTime = Date().addingTimeInterval(quoteTimer?.timeInterval ?? 30) // Update next quote time
+        updateNextQuoteDisplay()
     }
 
     @objc func quitApp() {
         NSApp.terminate(nil)
+    }
+
+    // MARK: - Refresh Menu
+    private func refreshMenu() {
+        setupMenuBar()
     }
 }
 
